01870075877b531fb117f27814ac2865c2885575
EAMoiseenko <s1lverhron@gmail.com>
2022-05-25 15:53:59 +0300

    The script is waiting for the document to be ready

--- src/widget/assets/js/summernote_widget.js
+++ src/widget/assets/js/summernote_widget.js
@@ -1,9 +1,11 @@
-'use strict';
-let textHtml = $(document.querySelector("[data-summernote]"))
+jQuery(document).ready(function() {
+    'use strict';
+    let textHtml = $(document.querySelector("[data-summernote]"))
 
-textHtml.summernote({
-    toolbar: textHtml.data('default-buttons'),
-    focus: true,
-    lang: 'ru-RU',
-    height: 200
+    textHtml.summernote({
+        toolbar: textHtml.data('default-buttons'),
+        focus: true,
+        lang: 'ru-RU',
+        height: 200
+    })
 })
