fe81ae340e4aea6addd853fa2027f50503eae627
s1lver <s1lverhron@gmail.com>
2023-03-27 14:10:10 +0300

    (#9) Added test

--- tests/Unit/WidgetTest.php
+++ tests/Unit/WidgetTest.php
@@ -4,12 +4,40 @@ declare(strict_types=1);
 
 namespace S1lver\Summernote\Tests\Unit;
 
+use s1lver\summernote\widget\helpers\SummernoteWidgetHelper;
+use s1lver\summernote\widget\SummernoteWidget;
 use PHPUnit\Framework\TestCase;
 
 class WidgetTest extends TestCase
 {
     public function testConfigureToolbarButtons(): void
     {
-        self::assertNull(null);
+        $customToolbar = [
+            [
+                'type' => 'dropdown',
+                'label' => 'Теги',
+                'tooltip' => 'Теги',
+                'values' => [
+                    'test1',
+                    'test2',
+                    'test3',
+                ]
+            ],
+        ];
+
+        $widgetHelper = new SummernoteWidgetHelper();
+
+        $result = $widgetHelper->configureToolbarButtons(
+            SummernoteWidget::DEFAULT_BUTTONS,
+            $customToolbar
+        );
+
+        self::assertIsArray($result);
+
+        foreach ($result as $item) {
+            if (is_string($item)) {
+                self::assertEquals('customDropdownButton', $item);
+            }
+        }
     }
 }
