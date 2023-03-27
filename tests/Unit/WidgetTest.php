0b9e86aa4bba2e5aec5b254684f0525121d17fa5
s1lver <s1lverhron@gmail.com>
2023-03-27 12:45:50 +0300

    Added example tests and mutation tests

---
+++ tests/Unit/WidgetTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace S1lver\Summernote\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class WidgetTest extends TestCase
+{
+    public function testConfigureToolbarButtons(): void
+    {
+        self::assertNull(null);
+    }
+}
